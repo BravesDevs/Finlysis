023175574a49bb3d332ebb8cc59169271ca85a1c
Devang Parekh <devangparekh2014@gmail.com>
2026-04-10 13:49:22 -0400

    Fix: Prisma config and load issues

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(configService: ConfigService) {
+    const adapter = new PrismaMariaDb(configService.getOrThrow<string>('DATABASE_URL'));
+    super({ adapter });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
